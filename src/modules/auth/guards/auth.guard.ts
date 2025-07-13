abd1b79618a186628de680de0c94670784063494
Mohammad Bekran <bhshtmhmd1@gmail.com>
2025-07-13 18:43:10 +0330

    feat(auth): implement auth guar & skip auth decorator
    - implement auth guard
    - implement custom skip auth decorator

---
+++ src/modules/auth/guards/auth.guard.ts
@@ -0,0 +1,50 @@
+import {
+  UnauthorizedException,
+  type CanActivate,
+  type ExecutionContext,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { isJWT } from 'class-validator';
+import type { Request } from 'express';
+
+import { SKIP_AUTH } from 'src/common/decorators/skip-auth.decorator';
+import { ENotAuthMessages } from 'src/common/enums/message.enum';
+
+import { AuthService } from '../auth.service';
+
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const isSkippedAuth = this.reflector.get<boolean>(
+      SKIP_AUTH,
+      context.getHandler(),
+    );
+    if (isSkippedAuth) return true;
+    const request = context.switchToHttp().getRequest<Request>();
+
+    const token = this.extractTokenFromRequest(request);
+    const user = await this.authService.validateAccessToken(token);
+
+    request.user = user;
+
+    return true;
+  }
+
+  protected extractTokenFromRequest(request: Request) {
+    const { authorization } = request.headers;
+    if (!authorization || authorization.trim() === '') {
+      throw new UnauthorizedException(ENotAuthMessages.LoginToAccount);
+    }
+
+    const [bearer, token] = authorization.split(' ');
+    if (bearer?.toLowerCase() !== 'bearer' || !token || !isJWT(token)) {
+      throw new UnauthorizedException(ENotAuthMessages.LoginToAccount);
+    }
+
+    return token;
+  }
+}
