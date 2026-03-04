c649cde2aa212cb043889c038ecd17ea6d174c0c
Mohammad Bekran <bhshtmhmd1@gmail.com>
2026-03-04 17:48:15 +0330

    chore: constant for api perfix

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerConfiguration } from './configs/swagger.config';
 import { AppModule } from './modules/app/app.module';
 
+const GLOBAL_API_PERFIX = 'api' as const;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   SwaggerConfiguration(app);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(GLOBAL_API_PERFIX);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
